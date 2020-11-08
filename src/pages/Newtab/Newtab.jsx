6673e2701c8b1ca5a28fdb1837cf5e5c6a8c31da
Ragav Kumar Valayapathy <rvalayapathy@zuora.com>
2020-11-08 18:39:09 +0530

    Getting meet link from calendar api

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/img/logo.svg';
+import { Button } from '@material-ui/core';
 import './Newtab.css';
 import './Newtab.scss';
+import dayjs from 'dayjs';
 
 const Newtab = () => {
-  chrome.identity.getAuthToken({ interactive: true }, function (token) {
-    console.log(token);
-  });
+  const [dateTime, setDateTime] = useState(new Date());
+  let authToken;
+
+  useEffect(() => {
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      console.log(token);
+      authToken = token;
+    });
+  }, []);
+
+  const meetNow = () => {
+    console.log('meetNow');
+    const event = {
+      summary: 'Meeting',
+      start: {
+        dateTime: dayjs().toISOString(),
+        timeZone: dayjs.tz.guess(),
+      },
+      end: {
+        dateTime: dayjs().add(30, 'minutes').toISOString(),
+        timeZone: dayjs.tz.guess(),
+      },
+      conferenceData: {
+        createRequest: {
+          requestId: Math.random().toString(36).substring(7),
+        },
+      },
+    };
+
+    fetch(
+      'https://www.googleapis.com/calendar/v3/calendars/primary/events?conferenceDataVersion=1&sendNotifications=true',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+      }
+    )
+      .then((response) => response.json())
+      .then(function (data) {
+        console.log(data);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
+        <div>
+          <Button color="primary" onClick={meetNow}>
+            Meet now
+          </Button>
+          <Button color="secondary">Schedule</Button>
+        </div>
       </header>
     </div>
   );
