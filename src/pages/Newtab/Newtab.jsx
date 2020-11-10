cb33776b4076ed09c696d64c95e1a55c6ce451e1
Ragav Kumar Valayapathy <rvalayapathy@zuora.com>
2020-11-10 11:55:19 +0530

    Added From date and End date picker

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../../assets/img/logo.svg';
-import { Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 import './Newtab.css';
 import './Newtab.scss';
 import dayjs from 'dayjs';
@@ -11,6 +15,8 @@ const Newtab = () => {
   const [dateTime, setDateTime] = useState(new Date());
   const [meetLink, setMeetLink] = useState('');
   const [authToken, setAuthToken] = useState('');
+  const [meetTitle, setMeetTitle] = useState('Meeting');
+  const [moreOptions, setMoreOptions] = useState(true);
 
   useEffect(() => {
     chrome.identity.getAuthToken({ interactive: true }, function (token) {
@@ -21,8 +27,9 @@ const Newtab = () => {
   const meetNow = () => {
     console.log('meetNow');
     console.log(authToken);
+    setMeetLink('Loading...');
     const event = {
-      summary: 'Meeting',
+      summary: meetTitle || '(No Title)',
       start: {
         dateTime: dayjs().toISOString(),
         timeZone: dayjs.tz.guess(),
@@ -56,16 +63,96 @@ const Newtab = () => {
       });
   };
 
+  const schedule = () => {
+    console.log('schedule');
+    console.log(authToken);
+    const event = {
+      summary: 'Scheduled Meeting',
+      start: {
+        dateTime: dayjs().toISOString(),
+        timeZone: dayjs.tz.guess(),
+      },
+      end: {
+        dateTime: dayjs().add(30, 'minutes').toISOString(),
+        timeZone: dayjs.tz.guess(),
+      },
+      conferenceData: {
+        createRequest: {
+          requestId: Math.random().toString(36).substring(7),
+        },
+      },
+    };
+
+    fetch(
+      'https://www.googleapis.com/calendar/v3/calendars/primary/events?conferenceDataVersion=1&sendNotifications=true',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+      }
+    )
+      .then((response) => response.json())
+      .then(function (data) {
+        console.log(data);
+        setMeetLink(data.hangoutLink);
+      });
+
+    // const validate = () => {
+    //   let temp = {};
+    //   temp.title = values.title ? '' : ''
+    // }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <Button color="primary" onClick={meetNow}>
-            Meet now
-          </Button>
-          <Button color="secondary">Schedule</Button>
-        </div>
+      <header>
+        <form className="App-header" noValidate autoComplete="off" error>
+          <FormControl>
+            <InputLabel>Title</InputLabel>
+            <Input
+              id="component-error"
+              value={meetTitle}
+              onChange={(e) => setMeetTitle(e.target.value)}
+              aria-describedby="component-error-text"
+            />
+          </FormControl>
+        </form>
+        <Button
+          color="secondary"
+          onClick={() => setMoreOptions((prev) => !prev)}
+        >
+          More Options
+        </Button>
+        {moreOptions && (
+          <div>
+            <TextField
+              id="datetime-local"
+              label="From"
+              type="datetime-local"
+              defaultValue="2017-05-24T10:30"
+              // className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              id="datetime-local"
+              label="To"
+              type="datetime-local"
+              defaultValue="2018-05-24T10:30"
+              // className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </div>
+        )}
+        <Button color="primary" onClick={meetNow}>
+          Meet now
+        </Button>
         <p>{meetLink || 'Start to Meet'}</p>
       </header>
     </div>
