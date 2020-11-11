930777f157c23ab11e70c2f702fd8bd194cd5425
Ragav Kumar Valayapathy <rvalayapathy@zuora.com>
2020-11-12 00:18:06 +0530

    Card design layout

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,11 +1,19 @@
 import { Button, TextField } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
+import Badge from '@material-ui/core/Badge';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
+import SettingsIcon from '@material-ui/icons/Settings';
+import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { LOAD_MEET, setAuthToken } from '../Background/actions';
 import { createMeet, getUserInfo } from '../Background/meetReducer';
-import { setAuthToken, LOAD_MEET } from '../Background/actions';
 import './Newtab.css';
 import './Newtab.scss';
 
@@ -29,6 +37,17 @@ const Newtab = () => {
     );
   };
 
+  const handleChangeUser = () => {
+    fetch(
+      'https://accounts.google.com/o/oauth2/revoke?token=' + authToken
+    ).then(() => {
+      chrome.identity.removeCachedAuthToken({ token: authToken }, () => {
+        dispatch(setAuthToken(null));
+        dispatch(getUserInfo());
+      });
+    });
+  };
+
   useEffect(() => {
     createMeetOnShortcut();
     dispatch(getUserInfo());
@@ -36,56 +55,91 @@ const Newtab = () => {
 
   return (
     <div className="App">
-      <header className="wrapper">
-        <Tooltip title={userInfo.email} placement="right">
-          <Avatar alt="Profile Pic" src={userInfo.picture} />
-        </Tooltip>
-        <TextField
-          label="Title"
-          type="input"
-          value={meetTitle}
-          className="form__title"
-          onChange={(e) => setMeetTitle(e.target.value)}
-          // className={classes.textField}
+      <Card>
+        <CardHeader
+          avatar={
+            <div onClick={handleChangeUser}>
+              <Tooltip title={userInfo.email} placement="right">
+                <Badge
+                  overlap="circle"
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  badgeContent={
+                    <SwapHorizIcon
+                      style={{
+                        backgroundColor: '#FFFF',
+                        borderRadius: '50%',
+                        boxShadow:
+                          '0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12)',
+                      }}
+                    />
+                  }
+                >
+                  <Avatar alt="Profile Pic" src={userInfo.picture} />
+                </Badge>
+              </Tooltip>
+            </div>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <SettingsIcon />
+            </IconButton>
+          }
+          title={userInfo.email}
         />
-        {moreOptions && (
-          <>
-            <TextField
-              id="datetime-local"
-              label="From"
-              type="datetime-local"
-              defaultValue="2017-05-24T10:30"
-              // className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-            <TextField
-              id="datetime-local"
-              label="To"
-              type="datetime-local"
-              defaultValue="2018-05-24T10:30"
-              // className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </>
-        )}
-        <Button
-          color="secondary"
-          onClick={() => setMoreOptions((prev) => !prev)}
-        >
-          {!moreOptions ? 'More' : 'Less'} Options
-        </Button>
-        <Button
-          color="primary"
-          onClick={() => dispatch(createMeet({ meetTitle }))}
-        >
-          {isLoading ? <CircularProgress /> : 'Meet now'}
-        </Button>
-        <p className="form__meet-link">{meetLink}</p>
-      </header>
+        <CardContent className="wrapper">
+          <TextField
+            label="Title"
+            type="input"
+            value={meetTitle}
+            className="form__title"
+            onChange={(e) => setMeetTitle(e.target.value)}
+            // className={classes.textField}
+          />
+          {moreOptions && (
+            <>
+              <TextField
+                id="datetime-local"
+                label="From"
+                type="datetime-local"
+                defaultValue="2017-05-24T10:30"
+                // className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                id="datetime-local"
+                label="To"
+                type="datetime-local"
+                defaultValue="2018-05-24T10:30"
+                // className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </>
+          )}
+          <p className="form__meet-link">{meetLink}</p>
+        </CardContent>
+        <CardActions>
+          <Button
+            color="secondary"
+            onClick={() => setMoreOptions((prev) => !prev)}
+          >
+            {!moreOptions ? 'More' : 'Less'} Options
+          </Button>
+          <Button
+            color="primary"
+            onClick={() => dispatch(createMeet({ meetTitle }))}
+            style={{ marginLeft: 'auto' }}
+          >
+            {isLoading ? <CircularProgress /> : 'Meet now'}
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
