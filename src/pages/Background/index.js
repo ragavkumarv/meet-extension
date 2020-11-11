f766d710d729e7234220b28a6ea5c39b3abe43c4
Ragav Kumar Valayapathy <rvalayapathy@zuora.com>
2020-11-11 13:01:15 +0530

    Loading Meet data with redux and thunk

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,68 @@
 import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+dayjs.extend(timezone);
 
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+chrome.commands.onCommand.addListener(function (command) {
+  console.log('Command:', command);
+  if (command === 'create-meeting')
+    chrome.runtime.sendMessage({
+      type: 'LOAD_MEET',
+    });
+});
+
+const initialState = { meetLink: '', authToken: '' };
+
+export const meetReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'loading':
+      return { ...state, meetLink: 'Loading...' };
+    case 'SET_AUTH_TOKEN':
+      return { ...state, authToken: action.payload };
+    case 'SET_MEET_LINK':
+      return { ...state, meetLink: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const loadMeet = ({ meetTitle }) => async (dispatch, getState) => {
+  dispatch({ type: 'loading' });
+
+  const event = {
+    summary: meetTitle || '(No Title)',
+    start: {
+      dateTime: dayjs().toISOString(),
+      timeZone: dayjs.tz.guess(),
+    },
+    end: {
+      dateTime: dayjs().add(30, 'minutes').toISOString(),
+      timeZone: dayjs.tz.guess(),
+    },
+    conferenceData: {
+      createRequest: {
+        requestId: Math.random().toString(36).substring(7),
+      },
+    },
+  };
+
+  const { hangoutLink } = await fetch(
+    'https://www.googleapis.com/calendar/v3/calendars/primary/events?conferenceDataVersion=1&sendNotifications=true',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${getState().authToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(event),
+    }
+  )
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+      return data;
+    });
+
+  dispatch({ type: 'SET_MEET_LINK', payload: hangoutLink });
+};
--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import logo from '../../assets/img/logo.svg';
 import { Button, TextField } from '@material-ui/core';
-import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import './Newtab.css';
-import './Newtab.scss';
-import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone';
 import Avatar from '@material-ui/core/Avatar';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Tooltip from '@material-ui/core/Tooltip';
-
-dayjs.extend(timezone);
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadMeet, loadShortCut } from '../Background/index';
+import './Newtab.css';
+import './Newtab.scss';
 
 const Newtab = () => {
-  const [dateTime, setDateTime] = useState(new Date());
-  const [meetLink, setMeetLink] = useState('');
-  const [authToken, setAuthToken] = useState('');
+  const dispatch = useDispatch();
+  const meetLink = useSelector((state) => state.meetLink);
+  const authToken = useSelector((state) => state.authToken);
   const [meetTitle, setMeetTitle] = useState('Meeting');
   const [moreOptions, setMoreOptions] = useState(true);
   const [userInfo, setUserInfo] = useState({ email: '', picture: '' });
 
   useEffect(() => {
-    chrome.identity.getAuthToken({ interactive: true }, function (token) {
-      setAuthToken(token);
+    window.chrome.runtime.onMessage.addListener(
+      (message, sender, sendResponse) => {
+        if (message.type === 'LOAD_MEET') {
+          console.log(meetTitle, authToken);
+          dispatch(loadMeet({ meetTitle }));
+        }
+      }
+    );
 
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      dispatch({ type: 'SET_AUTH_TOKEN', payload: token });
       fetch(
         `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${token}`
       )
@@ -37,112 +38,12 @@ const Newtab = () => {
     });
   }, []);
 
-  const meetNow = () => {
-    console.log('meetNow');
-    console.log(authToken);
-
-    setMeetLink('Loading');
-    const event = {
-      summary: meetTitle || '(No Title)',
-      start: {
-        dateTime: dayjs().toISOString(),
-        timeZone: dayjs.tz.guess(),
-      },
-      end: {
-        dateTime: dayjs().add(30, 'minutes').toISOString(),
-        timeZone: dayjs.tz.guess(),
-      },
-      conferenceData: {
-        createRequest: {
-          requestId: Math.random().toString(36).substring(7),
-        },
-      },
-    };
-
-    fetch(
-      'https://www.googleapis.com/calendar/v3/calendars/primary/events?conferenceDataVersion=1&sendNotifications=true',
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(event),
-      }
-    )
-      .then((response) => response.json())
-      .then(function (data) {
-        console.log(data);
-        setMeetLink(data.hangoutLink);
-      });
-  };
-
-  const schedule = () => {
-    console.log('schedule');
-    console.log(authToken);
-    const event = {
-      summary: 'Scheduled Meeting',
-      start: {
-        dateTime: dayjs().toISOString(),
-        timeZone: dayjs.tz.guess(),
-      },
-      end: {
-        dateTime: dayjs().add(30, 'minutes').toISOString(),
-        timeZone: dayjs.tz.guess(),
-      },
-      conferenceData: {
-        createRequest: {
-          requestId: Math.random().toString(36).substring(7),
-        },
-      },
-    };
-
-    fetch(
-      'https://www.googleapis.com/calendar/v3/calendars/primary/events?conferenceDataVersion=1&sendNotifications=true',
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(event),
-      }
-    )
-      .then((response) => response.json())
-      .then(function (data) {
-        console.log(data);
-        setMeetLink(data.hangoutLink);
-      });
-
-    // const validate = () => {
-    //   let temp = {};
-    //   temp.title = values.title ? '' : ''
-    // }
-  };
-
   return (
     <div className="App">
       <header className="wrapper">
-        {/* <form className="App-header" noValidate autoComplete="off" error>
-          <FormControl>
-            <InputLabel>Title</InputLabel>
-            <Input
-              id="component-error"
-              value={meetTitle}
-              onChange={(e) => setMeetTitle(e.target.value)}
-              aria-describedby="component-error-text"
-            />
-          </FormControl>
-        </form> */}
-        {/* <Avatar alt="Remy Sharp" src={userInfo.picture} /> */}
         <Tooltip title={userInfo.email} placement="right">
-          <Avatar
-            alt="Remy Sharp"
-            src={userInfo.picture}
-            // src={userInfo.picture}
-          />
+          <Avatar alt="Remy Sharp" src={userInfo.picture} />
         </Tooltip>
-        {/* <img src={userInfo.picture} alt="user pic" /> */}
         <TextField
           label="Title"
           type="input"
@@ -179,15 +80,16 @@ const Newtab = () => {
           color="secondary"
           onClick={() => setMoreOptions((prev) => !prev)}
         >
-          {moreOptions ? 'More' : 'Less'} Options
+          {!moreOptions ? 'More' : 'Less'} Options
         </Button>
-        <Button color="primary" onClick={meetNow}>
+        <Button
+          color="primary"
+          onClick={() => dispatch(loadMeet({ meetTitle }))}
+        >
           {meetLink === 'Loading' ? <CircularProgress /> : 'Meet now'}
         </Button>
         {/* <p className="form__meet-link">{meetLink || 'Start to Meet'}</p> */}
-        <p className="form__meet-link">
-          {meetLink !== 'Loading' ? meetLink : ''}
-        </p>
+        <p className="form__meet-link">{meetLink}</p>
       </header>
     </div>
   );
